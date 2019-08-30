3cf838f43c5e7c17307b8c0b055a37c89807001e
Mohammad Rahmani <rto1680@gmail.com>
2019-08-30 15:05:14 +0430

    Make login page and redirect functionality

---
+++ src/views/Login.vue
@@ -0,0 +1,90 @@
+<template>
+    <section class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+                <div class="login-panel bg-white text-left">
+                    <div class="alert alert-primary" role="alert" :style="{opacity: isAlertShow ? 1 : 0}">
+                        Login successfully. <small>waiting for redirect.</small>
+                        <loader-component width="30"></loader-component>
+                    </div>
+                    <h1 class="display-3 font-weight-bold">Login</h1>
+                    <p class="font-weight-bold">Welcome back</p>
+                    <br>
+                    <form action="">
+                        <div class="form-group">
+                            <lable class="input-label">Email</lable>
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <lable class="input-label">Password</lable>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+                        <br>
+                        <div class="form-group d-flex justify-content-center">
+                            <button class="btn btn-primary w-25" @click.prevent="login" v-if="!isLoggingIn">Login</button>
+                            <button class="btn btn-primary w-25" @click.prevent="login" v-if="isLoggingIn"><loader-component width="30"></loader-component></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import LoaderComponent from '../components/LoadingComponent'
+import { setTimeout } from 'timers';
+
+export default {
+    components: { LoaderComponent },
+    data() {
+        return {
+            isLoggingIn: false,
+            isAlertShow: false
+        }
+    },
+    methods: {
+        login() {
+            this.isLoggingIn = true
+            setTimeout(() => {
+                this.isLoggingIn = false
+                this.isAlertShow = true
+                setTimeout(() => this.redirect(), 1000)
+            }, 1000)
+        },
+
+        redirect() {
+            this.$router.push({name: 'home'})
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.widget {
+    margin: 0;
+    height: unset;
+}
+.login-panel {
+    position: relative;
+    padding: 200px 0;
+    .alert {
+        opacity: 0;
+        position: absolute;
+        width: 100%;
+        top: 100px;
+        &.alert-primary {
+            background-color: #007BFF;
+            color: #fff;
+            font-size: 18px;
+            border: none;
+        }
+        .widget {
+            position: absolute;
+            right: 5px;
+            top: 0;
+            margin: 10px;
+        }
+    }
+}
+</style>
